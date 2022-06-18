c9313b454a5e74ec72b59b82106571a1eae68369
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-18 16:41:17 +0300

    commit tree 784ab8dfcfb7eb947b89635d97a4ed47666a22e7

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (n = 0; n < word.length/2; n++ ){
+
+    let o = word.length -1-n;
+
+    if (word[n] === word[o]){
+      return true;
+    }
+    return false 
+  }
+  
 }
 
 /* 
   Add your pseudocode here
 */
+// iterate from the beginning to the middle of the string and compare the letters of the forst half to the letters in the second half
 
+// if the letters from the end equals to the letters from the beginning, return true
+// else, return false
 /*
   Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
